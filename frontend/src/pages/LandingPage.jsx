b605a529a56e951fb05e5866daaf73eb74f0a77d
PRABAKARAN S B <155632107+PRABU-21@users.noreply.github.com>
2025-09-20 10:49:22 +0530

    Update LandingPage.jsx

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,11 +1,12 @@
 import Recommend from "../components/Recommend";
 import Parser from "../components/parser";
-
+import MatchCandidates from "../components/MatchCandidates";
 function LandingPage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-50 via-white to-purple-100 flex flex-col items-center justify-start py-10 gap-10">
       <Recommend />
       <Parser />
+      <MatchCandidates />
     </div>
   );
 }
